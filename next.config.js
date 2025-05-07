e60e2cc56dd793c6fff7a25a53d41ec51ea23363
ben goldshtein <>
2025-05-07 19:21:47 +0300

    reverty build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    distDir: 'build',
     webpack: (config, { isServer }) => {
       config.devtool = 'source-map';
         if (isServer) {
